7cfeee46256f57cb24c5a54e225b18c76ec27f9c
csamk <csamk@SUMMRSXO>
2025-01-09 09:11:38 +0500

    реализована папка с классами

---
+++ Classes/DataBase/Connection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace WpfApp1.Classes.DataBase
+{
+    public class Connection
+    {
+        private static readonly string config = "server=HOME-PC\\MYSERVER;" +
+            "Trusted_Connection=No;" +
+            "DataBase=BooksAndAuthors;" +
+            "User=sa;" +
+            "PWD=sa;";
+        public static SqlConnection OpenConnection()
+        {
+            SqlConnection connection = new SqlConnection(config);
+            connection.Open();
+            return connection;
+        }
+        public static SqlDataReader Query(string sql, out SqlConnection connection)
+        {
+            connection = OpenConnection();
+            return new SqlCommand(sql, connection).ExecuteReader();
+        }
+        public static void CloseConnection(SqlConnection connection)
+        {
+            connection.Close();
+            SqlConnection.ClearPool(connection);
+        }
+    }
+}
