14a12cd91e97457d5aa2e05ee1019d3d2870ee26
csamk <csamk@SUMMRSXO>
2025-01-09 09:22:14 +0500

    сделана папка с моделями

---
+++ Context/AuthorContext.cs
@@ -0,0 +1,139 @@
+﻿using WpfApp1.Classes;
+using WpfApp1.Classes.DataBase;
+using WpfApp1.Modell;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Windows;
+
+namespace WpfApp1.Context
+{
+    public class AuthorsContext : Authors
+    {
+        private bool _isNew = true;
+        public AuthorsContext(bool save = false)
+        {
+            if (save) save = true;
+        }
+        public static ObservableCollection<AuthorsContext> AllAuthors()
+        {
+            ObservableCollection<AuthorsContext> allAuthors = new ObservableCollection<AuthorsContext>();
+            SqlConnection connection;
+            SqlDataReader dataItems = Connection.Query("Select * from [dbo].[Authors]", out connection);
+            while (dataItems.Read())
+            {
+                allAuthors.Add(new AuthorsContext()
+                {
+                    Id = dataItems.GetInt32(0),
+                    Surname = dataItems.GetString(1),
+                    Name = dataItems.GetString(2),
+                    Lastname = dataItems.GetString(3),
+                });
+            }
+            Connection.CloseConnection(connection);
+            return allAuthors;
+        }
+        public void Save()
+        {
+            SqlConnection connection;
+            if (_isNew)
+            {
+                try
+                {
+                    SqlDataReader dataItems = Connection.Query("Insert into " +
+                        "[dbo].[Authors](" +
+                        "Surname, " +
+                        "Name, " +
+                        "Lastname) " +
+                        "OUTPUT Inserted.Id " +
+                        "Values (" +
+                        $"N'{Surname}', " +
+                        $"N'{Name}', " +
+                        $"N'{Lastname}')", out connection);
+                    dataItems.Read();
+                    Id = dataItems.GetInt32(0);
+                    Connection.CloseConnection(connection);
+                    MessageBox.Show("Действие выполнено!", "Уведмление");
+                }
+                catch
+                {
+                    MessageBox.Show("Не удалось выполнить действие!", "Уведмление");
+                }
+            }
+            else
+            {
+                try
+                {
+                    Connection.Query("Update [dbo].[Authors] " +
+                        "Set " +
+                        $"Surname = N'{Surname}', " +
+                        $"Name = N'{Name}', " +
+                        $"Lastname = N'{Lastname}' " +
+                        "Where " +
+                        $"Id = {Id}", out connection);
+                    Connection.CloseConnection(connection);
+                }
+                catch
+                {
+                    MessageBox.Show("Не удалось выполнить действие!", "Уведмление");
+                }
+            }
+            _isNew = true;
+            MainWindow.MW.frame.Navigate(MainWindow.Main);
+        }
+        private void Delete(bool withBooks)
+        {
+            SqlConnection connection;
+            if (withBooks)
+            {
+                Connection.Query("Delete from [dbo].[Books] " +
+                            "Where " +
+                            $"Author = {Id}", out connection);
+            }
+            Connection.Query("Delete from [dbo].[Authors] " +
+                "Where " +
+                $"Id = {Id}", out connection);
+            Connection.CloseConnection(connection);
+            (MainWindow.Main.Authors.DataContext as ViewModell.VMAuthors).Authors.Remove(this);
+            MessageBox.Show("Действие выполнено!", "Уведмление");
+        }
+        public RelayCommand OnEdit
+        {
+            get
+            {
+                _isNew = false;
+                return new RelayCommand(obj => MainWindow.MW.frame.Navigate(new View.AddAuthors(this)));
+            }
+        }
+        public RelayCommand OnSave
+        {
+            get
+            {
+                return new RelayCommand(obj =>
+                {
+                    Save();
+                });
+            }
+        }
+        public RelayCommand OnDelete
+        {
+            get
+            {
+                return new RelayCommand(obj =>
+                {
+                    if (MessageBox.Show("Вы уверены, что хотите удалить этого автора?", "Уведомление", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                    {
+                        if (BooksContext.AllBooks().Where(x => x.Author.Id == this.Id).ToList().Count > 0)
+                        {
+                            if (MessageBox.Show("У этого автора есть книги, вы действительно хотите удалить его?", "Уведомление", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                            {
+                                Delete(true);
+                            }
+                        }
+                        else Delete(false);
+                    }
+                });
+            }
+        }
+    }
+}
---
+++ Context/BooksContext.cs
@@ -0,0 +1,260 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using static System.Reflection.Metadata.BlobBuilder;
+using System.Windows;
+using System.Xml.Linq;
+using WpfApp1.Classes.DataBase;
+using WpfApp1.Classes;
+
+namespace WpfApp1.Context
+{
+    public class BooksContext : Books
+    {
+        private bool _isNew = true;
+        public BooksContext(bool save = false)
+        {
+            if (save) save = true;
+            Author = new Authors();
+        }
+        public static ObservableCollection<BooksContext> AllBooks()
+        {
+            ObservableCollection<BooksContext> allBooks = new ObservableCollection<BooksContext>();
+            ObservableCollection<AuthorsContext> allAuthors = AuthorsContext.AllAuthors();
+            SqlConnection connection;
+            SqlDataReader dataItems = Connection.Query("Select * from [dbo].[Books]", out connection);
+            while (dataItems.Read())
+            {
+                allBooks.Add(new BooksContext()
+                {
+                    Id = dataItems.GetInt32(0),
+                    Name = dataItems.GetString(1),
+                    Author = dataItems.IsDBNull(2) ? null : allAuthors.Where(x => x.Id == dataItems.GetInt32(2)).First(),
+                    Description = dataItems.GetString(3),
+                });
+            }
+            Connection.CloseConnection(connection);
+            return allBooks;
+        }
+        public void Save()
+        {
+            SqlConnection connection;
+            if (_isNew)
+            {
+                try
+                {
+                    SqlDataReader dataItems = Connection.Query("Insert into " +
+                        "[dbo].[Books](" +
+                        "Name, " +
+                        "Author, " +
+                        "Description) " +
+                        "OUTPUT Inserted.Id " +
+                        "Values (" +
+                        $"N'{Name}', " +
+                        $"{Author.Id}, " +
+                        $"N'{Description}')", out connection);
+                    dataItems.Read();
+                    Id = dataItems.GetInt32(0);
+                    Connection.CloseConnection(connection);
+                    MessageBox.Show("Действие выполнено!", "Уведмление");
+                }
+                catch
+                {
+                    MessageBox.Show("Не удалось выполнить действие!", "Уведмление");
+                }
+            }
+            else
+            {
+                try
+                {
+                    Connection.Query("Update [dbo].[Books] " +
+                        "Set " +
+                        $"Name = N'{Name}', " +
+                        $"Author = {Author.Id}, " +
+                        $"Description = N'{Description}' " +
+                        "Where " +
+                        $"Id = {Id}", out connection);
+                    Connection.CloseConnection(connection);
+                    MessageBox.Show("Действие выполнено!", "Уведмление");
+                }
+                catch
+                {
+                    MessageBox.Show("Не удалось выполнить действие!", "Уведмление");
+                }
+            }
+            MainWindow.MW.frame.Navigate(MainWindow.Main);
+        }
+        public void Delete()
+        {
+            try
+            {
+                SqlConnection connection;
+                Connection.Query("Delete from [dbo].[Books] " +
+                    "Where " +
+                    $"Id = {Id}", out connection);
+                Connection.CloseConnection(connection);
+                MessageBox.Show("Действие выполнено!", "Уведмление");
+            }
+            catch
+            {
+                MessageBox.Show("Не удалось выполнить действие!", "Уведмление");
+            }
+        }
+        public RelayCommand OnEdit
+        {
+            get
+            {
+                _isNew = false;
+                return new RelayCommand(obj => MainWindow.MW.frame.Navigate(new View.AddBook(this)));
+            }
+        }
+        public RelayCommand OnSave
+        {
+            get
+            {
+                return new RelayCommand(obj =>
+                {
+                    Author = AuthorsContext.AllAuthors().Where(x => x.Id == Author.Id).First();
+                    Save();
+                    _isNew = true;
+                });
+            }
+        }
+        public RelayCommand OnDelete
+        {
+            get
+            {
+                return new RelayCommand(obj =>
+                {
+                    Delete();
+                    (MainWindow.Main.Books.DataContext as ViewModell.VMBooks).Books.Remove(this);
+                });
+            }
+        }
+    }
+    public class BooksContext : Books
+    {
+        private bool _isNew = true;
+        public BooksContext(bool save = false)
+        {
+            if (save) save = true;
+            Author = new Authors();
+        }
+        public static ObservableCollection<BooksContext> AllBooks()
+        {
+            ObservableCollection<BooksContext> allBooks = new ObservableCollection<BooksContext>();
+            ObservableCollection<AuthorsContext> allAuthors = AuthorsContext.AllAuthors();
+            SqlConnection connection;
+            SqlDataReader dataItems = Connection.Query("Select * from [dbo].[Books]", out connection);
+            while (dataItems.Read())
+            {
+                allBooks.Add(new BooksContext()
+                {
+                    Id = dataItems.GetInt32(0),
+                    Name = dataItems.GetString(1),
+                    Author = dataItems.IsDBNull(2) ? null : allAuthors.Where(x => x.Id == dataItems.GetInt32(2)).First(),
+                    Description = dataItems.GetString(3),
+                });
+            }
+            Connection.CloseConnection(connection);
+            return allBooks;
+        }
+        public void Save()
+        {
+            SqlConnection connection;
+            if (_isNew)
+            {
+                try
+                {
+                    SqlDataReader dataItems = Connection.Query("Insert into " +
+                    "[dbo].[Books](" +
+                        "Name, " +
+                        "Author, " +
+                        "Description) " +
+                        "OUTPUT Inserted.Id " +
+                        "Values (" +
+                        $"N'{Name}', " +
+                        $"{Author.Id}, " +
+                        $"N'{Description}')", out connection);
+                    dataItems.Read();
+                    Id = dataItems.GetInt32(0);
+                    Connection.CloseConnection(connection);
+                    MessageBox.Show("Действие выполнено!", "Уведмление");
+                }
+                catch
+                {
+                    MessageBox.Show("Не удалось выполнить действие!", "Уведмление");
+                }
+            }
+            else
+            {
+                try
+                {
+                    Connection.Query("Update [dbo].[Books] " +
+                        "Set " +
+                        $"Name = N'{Name}', " +
+                        $"Author = {Author.Id}, " +
+                        $"Description = N'{Description}' " +
+                        "Where " +
+                        $"Id = {Id}", out connection);
+                    Connection.CloseConnection(connection);
+                    MessageBox.Show("Действие выполнено!", "Уведмление");
+                }
+                catch
+                {
+                    MessageBox.Show("Не удалось выполнить действие!", "Уведмление");
+                }
+            }
+            MainWindow.MW.frame.Navigate(MainWindow.Main);
+        }
+        public void Delete()
+        {
+            try
+            {
+                SqlConnection connection;
+                Connection.Query("Delete from [dbo].[Books] " +
+                    "Where " +
+                    $"Id = {Id}", out connection);
+                Connection.CloseConnection(connection);
+                MessageBox.Show("Действие выполнено!", "Уведмление");
+            }
+            catch
+            {
+                MessageBox.Show("Не удалось выполнить действие!", "Уведмление");
+            }
+        }
+        public RelayCommand OnEdit
+        {
+            get
+            {
+                _isNew = false;
+                return new RelayCommand(obj => MainWindow.MW.frame.Navigate(new View.AddBook(this)));
+            }
+        }
+        public RelayCommand OnSave
+        {
+            get
+            {
+                return new RelayCommand(obj =>
+                {
+                    Author = AuthorsContext.AllAuthors().Where(x => x.Id == Author.Id).First();
+                    Save();
+                    _isNew = true;
+                });
+            }
+        }
+        public RelayCommand OnDelete
+        {
+            get
+            {
+                return new RelayCommand(obj =>
+                {
+                    Delete();
+                    (MainWindow.Main.Books.DataContext as ViewModell.VMBooks).Books.Remove(this);
+                });
+            }
+        }
+    }
+}
